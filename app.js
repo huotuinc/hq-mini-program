ac712886057baac360469b2a20ff91a61cb07519
guomw <516985724@qq.com>
2018-05-29 17:57:05 +0800

    add

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ App({
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
-    
+    wx.login({
+      success:function(res){
+        console.log(res);
+      }
+    })
   },
 
   /**
