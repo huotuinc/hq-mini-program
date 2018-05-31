afde7f55d8202519551ac243db838231917e8e65
sjw0608 <3486106508@qq.com>
2018-05-31 19:53:23 +0800

    修改&添加

---
+++ pages/revenueCenter/revenueCenter.js
@@ -0,0 +1,129 @@
+var wxCharts = require('../../utils/wxcharts-min.js');
+var app = getApp();
+var lineChart = null;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  touchHandler: function (e) {
+    console.log(lineChart.getCurrentDataIndex(e));
+    lineChart.showToolTip(e, {
+      // background: '#7cb5ec',
+      format: function (item, category) {
+        return category + ' ' + item.name + ':' + item.data
+      }
+    });
+  },
+
+  createSimulationData: function () {
+    var categories = [];
+    var data = [];
+    var newTime = new Date();
+    var month = newTime.getMonth()+1;
+    var day = newTime.getDate();
+    for (var i = 0; i < 7; i++) {
+      categories.push(month+"/"+(day-i));
+    }
+    return {
+      categories: categories,
+      data: data
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var windowWidth = 300;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth;
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
+
+    var simulationData = this.createSimulationData();
+    lineChart = new wxCharts({
+      canvasId: 'lineCanvas',
+      type: 'line',
+      categories: simulationData.categories,
+      animation: true,
+      series: [{
+        name: '本周期',
+        data: [1,0,0,2,2,2,0],
+        format: function (val, name) {
+          return val.toFixed(2) ;
+        }
+      }, {
+        name: '上周期',
+        data: [2, 0, 0, 3, 0, 2, 0],
+        format: function (val, name) {
+          return val.toFixed(2);
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
