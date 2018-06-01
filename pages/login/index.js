5caae8418a5aadd4a13fb1de3aa50dfff4525501
guomw <516985724@qq.com>
2018-06-01 17:56:34 +0800

    添加登录页面

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,4 +1,4 @@
-// pages/welfare/index.js
+// pages/login/index.js
 Page({
 
   /**
@@ -12,9 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.navigateTo({
-      url: '../login/index',
-    })
+  
   },
 
   /**
