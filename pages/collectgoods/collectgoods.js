f37de36572084edc180c808eff3c95547bda28e6
sjw0608 <3486106508@qq.com>
2018-06-05 10:20:49 +0800

    收藏夹 全选/全不选/部分选中 完善

--- pages/collectgoods/collectgoods.js
+++ pages/collectgoods/collectgoods.js
@@ -1,31 +1,67 @@
 const indexData = require('../../utils/mock/index.js')
 Page({
   data: {
-    checked: false
+    checked: false,
+    checkedTitle: '已选 （0）'
   },
   onLoad: function (options) {
     this.setData({
       hotItems: indexData.collectItems
     });
   },
-
+  // 选中和不选中
   clickFavTab: function (e) {
     var item = e.currentTarget.dataset.item
     var index = e.currentTarget.dataset.index
-    var _type = e.currentTarget.dataset.type
-    var _items = []
-    if (_type == 'goodsItems') {
-      _items = this.data.goodsItems
-      _items[index].isFav = !item.isFav
+    var _items = this.data.hotItems
+    _items[index].isFav = !item.isFav
+    this.setData({
+      hotItems: _items
+    })
+    let num = 0
+    for (var i = 0; i < _items.length; i++) {
+      if (!_items[i].isFav) {
+        num++
+      }
+    }
+    if (num < _items.length) {
       this.setData({
-        goodsItems: _items,
+        checked: false,
+        checkedTitle: '已选（' + num + '）'
+      })
+    } else {
+      this.setData({
+        checked: true,
+        checkedTitle: '全选（' + num + '）'
       })
     }
-    if (_type == 'hotItems') {
-      _items = this.data.hotItems
-      _items[index].isFav = !item.isFav
+  },
+  
+  // 全选全部选
+  checkedAll: function (e) {
+    var _items = this.data.hotItems
+    var check = this.data.checked
+    if (!check) {
+      for (var i = 0; i < _items.length; i++) {
+        if (_items[i].isFav) {
+          _items[i].isFav = false
+        }
+      }
       this.setData({
-        hotItems: _items
+        hotItems: _items,
+        checked: true,
+        checkedTitle: '全选（' + _items.length + '）'
+      })
+    } else {
+      for (var i = 0; i < _items.length; i++) {
+        if (!_items[i].isFav) {
+          _items[i].isFav = true
+        }
+      }
+      this.setData({
+        hotItems: _items,
+        checked: false,
+        checkedTitle: '已选（0）'
       })
     }
   },
