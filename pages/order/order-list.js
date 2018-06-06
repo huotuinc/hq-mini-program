858355ebae603bd431df2de041a97d37424bc6f8
guomw <516985724@qq.com>
2018-06-06 11:44:03 +0800

    修改

--- pages/order/order-list.js
+++ pages/order/order-list.js
@@ -83,11 +83,7 @@ Page({
   
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  },
+
 
   /**
    * 页面上拉触底事件的处理函数
@@ -99,12 +95,6 @@ Page({
     this.setData({
       orders: _orders
     })
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
+
 })
