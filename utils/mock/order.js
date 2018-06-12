097c21cd00c5c4a794f3645ef21434f5a7f093c8
sjw0608 <3486106508@qq.com>
2018-06-12 15:13:21 +0800

    add & 修改

--- utils/mock/order.js
+++ utils/mock/order.js
@@ -1,32 +1,32 @@
 
-const orderList=[
+const orderList = [
+  // {
+  //   userName: '星期八',
+  //   orderId: '2018060214162515',/**订单号 */
+  //   orderCreateTime: '2018-06-01',/**下单时间 */
+  //   imgSrc: 'http://t00img.yangkeduo.com/t05img/images/2018-05-29/af95c46dc07e8a0a74d2cbf800fbd07d.jpeg',/**图片 */
+  //   goodsName: '正品清脂流茶',/**标题 */
+  //   orderAmount: 19.90,/**消费金额 */
+  //   orderStatus: 0,/**订单状态 */
+  //   orderGroupCuccessTime: '2018-06-01'
+  // },
+  // {
+  //   userName: '星期八',
+  //   orderId: '2018060214162515',/**订单号 */
+  //   orderCreateTime: '2018-06-01',/**下单时间 */
+  //   imgSrc: 'http://t05img.yangkeduo.com/images/2018-05-04/763d35d3060a752f6b296cf0db710094.jpeg',/**图片 */
+  //   goodsName: '孕婴面巾纸300张/包',/**标题 */
+  //   orderAmount: 26.90,/**消费金额 */
+  //   orderStatus: 0,/**订单状态 */
+  //   orderGroupCuccessTime: '2018-06-01',
+  // },
   {
-      orderId:'2018060214162515',/**订单号 */
-      orderCreateTime:'2018-06-01',/**下单时间 */
-      imgSrc:'http://t00img.yangkeduo.com/t05img/images/2018-05-29/af95c46dc07e8a0a74d2cbf800fbd07d.jpeg',/**图片 */
-      goodsName:'[减肥茶月瘦5-30斤]华佗拾遗瘦肚子瘦身产品瘦腿神器正品清脂流茶',/**标题 */
-      orderAmount:19.90,/**消费金额 */
-      promotionAmount:1.25,/**佣金额度 */
-      orderStatus:0,/**订单状态 */
-      orderGroupCuccessTime:'2018-06-01'
-  },
-  {
+    userName: '星期八',
     orderId: '2018060214162515',/**订单号 */
     orderCreateTime: '2018-06-01',/**下单时间 */
     imgSrc: 'http://t05img.yangkeduo.com/images/2018-05-04/763d35d3060a752f6b296cf0db710094.jpeg',/**图片 */
-    goodsName: '【24包超值装 】沐曦原生木浆抽取式孕婴面巾纸300张/包',/**标题 */
+    goodsName: '孕婴面巾纸300张/包',/**标题 */
     orderAmount: 26.90,/**消费金额 */
-    promotionAmount: 3.25,/**佣金额度 */
-    orderStatus: 0,/**订单状态 */
-    orderGroupCuccessTime: '2018-06-01'
-  },
-  {
-    orderId: '2018060214162515',/**订单号 */
-    orderCreateTime: '2018-06-01',/**下单时间 */
-    imgSrc: 'http://t05img.yangkeduo.com/images/2018-05-04/763d35d3060a752f6b296cf0db710094.jpeg',/**图片 */
-    goodsName: '【24包超值装 】沐曦原生木浆抽取式孕婴面巾纸300张/包',/**标题 */
-    orderAmount: 26.90,/**消费金额 */
-    promotionAmount: 3.25,/**佣金额度 */
     orderStatus: 0,/**订单状态 */
     orderGroupCuccessTime: '2018-06-01'
   }
